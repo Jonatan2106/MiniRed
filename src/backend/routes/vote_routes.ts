0b8f3ff34566ed2589042e9f1adb23405afad14c
StanislausJoel <stanislausjoel.fa@gmail.com>
2025-04-25 19:31:48 +0700

    add vote route

--- src/backend/routes/vote_routes.ts
+++ src/backend/routes/vote_routes.ts
@@ -3,10 +3,7 @@ import { voteOnPost, voteOnComment } from '../controllers/vote_controller';
 
 const VoteRouter = express.Router();
 
-// Vote on a post
 VoteRouter.post('/posts/:id/votes', voteOnPost);
-
-// Vote on a comment
 VoteRouter.post('/comments/:id/votes', voteOnComment);
 
 export default VoteRouter;
