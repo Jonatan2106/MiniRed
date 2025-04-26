54f9349100702f092ab037e3e957623e2ce50c60
StanislausJoel <stanislausjoel.fa@gmail.com>
2025-04-26 17:34:37 +0700

    fix vote API

--- src/backend/routes/vote_routes.ts
+++ src/backend/routes/vote_routes.ts
@@ -1,10 +1,20 @@
 import express from 'express';
-import { voteOnPost, voteOnComment } from '../controllers/vote_controller';
+import { voteOnPost, voteOnComment, updateVote, deleteVote, getCommentVotesCount, getPostVotesCount, getVotesForPost, getVotesForComment, getUpvotesForPost, getDownvotesForPost, getVotesByUserForPost } from '../controllers/vote_controller';
 import { authenticateJWT } from '../middleware/auth_middleware';
 
 const VoteRouter = express.Router();
 
 VoteRouter.post('/posts/:id/votes', authenticateJWT, voteOnPost);
+VoteRouter.get('/posts/:id/votes/count', authenticateJWT, getPostVotesCount);
 VoteRouter.post('/comments/:id/votes', authenticateJWT, voteOnComment);
+VoteRouter.get('/comments/:id/votes/count', authenticateJWT, getCommentVotesCount);
+VoteRouter.put('/votes/:id', authenticateJWT, updateVote);
+VoteRouter.delete('/votes/:id', authenticateJWT, deleteVote);
+VoteRouter.get('/posts/:id/votes', authenticateJWT, getVotesForPost);
+VoteRouter.get('/comments/:id/votes', authenticateJWT, getVotesForComment);
+VoteRouter.get('/posts/:id/votes/upvotes', authenticateJWT, getUpvotesForPost);
+VoteRouter.get('/posts/:id/votes/downvotes', authenticateJWT, getDownvotesForPost);
+VoteRouter.get('/posts/:id/votes/user/:userId', authenticateJWT, getVotesByUserForPost);
+
 
 export default VoteRouter;
