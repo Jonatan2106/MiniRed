a1beb71f390a5f7222faf0f23d570d8393de2771
StanislausJoel <stanislausjoel.fa@gmail.com>
2025-04-26 19:26:45 +0700

    undo comment & search

--- src/backend/routes/search_routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { searchPosts } from '../controllers/search_controller';
-
-const SearchRouter = Router();
-
-// Search for posts by keyword
-SearchRouter.get('/search', searchPosts);
-
-export default SearchRouter;
